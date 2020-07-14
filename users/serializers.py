75495f078d3c29d2228323856a021552f93ea550
Sarah Grossman <sarah@pillar.app>
2020-07-14 18:58:22 -0400

    add bicycles to user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+
+from bicycles.serializers import BicycleSerializer
 from users.models import User
 
 
 # Serializers define the API representation.
+
 class UserSerializer(serializers.ModelSerializer):
+    bicycles = BicycleSerializer(many=True, read_only=True, source='bicycle_set')
+    
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff', 'id']
+        fields = ['username', 'email', 'is_staff', 'id', 'bicycles']
